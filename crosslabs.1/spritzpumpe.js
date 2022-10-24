7cf978a8edbf703d78457eb5fdf18daa62a8cb0d
StijnDVRS <116549459+StijnDVRS@users.noreply.github.com>
2022-10-24 10:17:31 +0200

    Update spritzpumpe.js

--- crosslabs.1/spritzpumpe.js
+++ crosslabs.1/spritzpumpe.js
@@ -9,6 +9,30 @@ Blockly.Blocks['spritzpumpe'] = {
  this.setHelpUrl("");
   }
 };
+Blockly.Blocks['lichtsensor'] = {
+  init: function() {
+    this.appendValueInput("Lichtwert")
+        .setCheck("Number")
+        .appendField("Lichtsensor");
+    this.setPreviousStatement(true, null);
+    this.setColour(330);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['geschwindigkeit'] = {
+  init: function() {
+    this.appendValueInput("Geschwindigkeit")
+        .setCheck("")
+        .appendField(new Blockly.FieldDropdown([["Langsam","Langsam"], ["Schnell","Schnell"], ["Normal","Normal"]]), "NAME")
+        .appendField("Geschwindigkeit");
+    this.setOutput(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
 
 //Generated stub
 
@@ -16,4 +40,19 @@ Blockly.JavaScript['spritzpumpe'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
   return code;
+};
+Blockly.JavaScript['lichtsensor'] = function(block) {
+  var value_lichtwert = Blockly.JavaScript.valueToCode(block, 'Lichtwert', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['geschwindigkeit'] = function(block) {
+  var dropdown_name = block.getFieldValue('NAME');
+  var value_geschwindigkeit = Blockly.JavaScript.valueToCode(block, 'Geschwindigkeit', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
