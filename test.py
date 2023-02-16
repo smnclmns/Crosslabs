1fb7154b06e836a6008eef211256a9867b3511ea
StijnDV <Stijn.devries@tu-dortmund.de>
2023-02-16 16:33:03 +0200

    Groß und kleinschreibung ist egal bbbyby

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 with open("UMLs/txt_files/Calibration.txt","r") as cal:
-    caldata = cal.read().replace('\n', '')
+    caldata = cal.read().replace('\n', '').lower()
 
 with open("UMLs/txt_files/v1.txt", "r") as v1:
-    v1data = v1.read().replace('\n', '')
+    v1data = v1.read().replace('\n', '').lower()
 
 print(v1data==caldata)
