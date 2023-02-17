2d210975997ab7a0ffc5e46d794d02d2fdab2e8e
smnclmns <116153620+smnclmns@users.noreply.github.com>
2023-02-17 08:48:15 +0100

    Check()-Function mit score

--- app.py
+++ app.py
@@ -2,20 +2,26 @@ from flask import Flask, render_template, request, flash
 from UMLs.uml_handling import UML_Handler, get_ids
 
 def check():
+
     with open("UMLs/txt_files/Calibration.txt","r") as cal:
         vorlage = cal.read().split("/n")
-
 
     with open("UMLs/txt_files/v1.txt", "r") as v1:
         entwurf = v1.read().split("/n")
 
-    for i,line in enumerate(vorlage):
-        if i >= 
+    score = 0
 
-     
+    for i,line in enumerate(entwurf):
 
-    statement = v1data==caldata
-    return statement
+        entwurf_line = line.replace(" ", "")
+
+        # if line is right: score += 1
+        
+        pass
+
+
+    
+    return score
 
 
 app = Flask(__name__)
