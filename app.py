6f2aca578430c21c1a01f62f6656bb9d7f82e1b9
StijnDV <Stijn.devries@tu-dortmund.de>
2023-02-17 09:31:07 +0200

    call

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template, request, flash
 from UMLs.uml_handling import UML_Handler, get_ids
 
+def check():
+    with open("UMLs/txt_files/Calibration.txt","r") as cal:
+        vorlage = cal.read().split("/n")
+
+
+    with open("UMLs/txt_files/v1.txt", "r") as v1:
+        entwurf = v1.read().split("/n")
+
+    for i,line in enumerate(vorlage):
+        if i >= 
+
+     
+
+    statement = v1data==caldata
+    return statement
+
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
@@ -12,11 +29,13 @@ def home():
         text_input = handler.get_plantuml_text("v1")
 
     handler.add_uml_file(text_input)
-    import test 
-    from test import statement
+
+    
+    
    
     
-    return render_template("index.html", uml_src=handler.get_plantuml_url("v1", "svg"), state = statement)
+    return render_template("index.html", uml_src=handler.get_plantuml_url("v1", "svg"), state = check())
+
 
 
 
