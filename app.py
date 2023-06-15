91688fe9fc4dc26e5105575a624c3880e7b0a0a1
smnclmns <116153620+smnclmns@users.noreply.github.com>
2023-06-15 10:52:09 +0200

    level temlpates erstellt

--- app.py
+++ app.py
@@ -69,6 +69,161 @@ def tutorial():
      tips=tips,
     )
 
+@app.route("/workspace/level1", methods=["POST", "GET"])
+def level1():
+
+    input_dict = {
+        "Tut_calibration_steps": 0,
+        "Tut_calibration_delay": 0,
+        }
+    
+    tips= ""
+    score = 0
+
+    if request.method == "POST":
+        
+        for key, val in input_dict.items():
+
+            inp = request.form.get(key)
+            if inp == None: continue
+            else: input_dict[key] = inp
+
+    for key, val in input_dict.items():
+        
+        if key == "Tut_calibration_steps":
+            if val != "500": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            else: score += 1
+
+        if key == "Tut_calibration_delay":
+            if val != "1000": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            else: score += 1
+
+    return render_template("Levels/level1.html",
+        uml_src=url_for('static', filename='assets/SVGs/Level1.svg'),
+        value=score,
+        tips=tips,
+    )
+
+@app.route("/workspace/level2", methods=["POST", "GET"])
+def level2():
+
+    input_dict = {
+        "Tut_iterations": 0,
+        "Tut_steps": 0,
+        "Tut_delay": 0,
+    }
+
+    tips= ""
+    score = 0
+
+    if request.method == "POST":
+
+        for key, val in input_dict.items():
+
+            inp = request.form.get(key)
+            if inp == None: continue
+            else: input_dict[key] = inp
+
+    for key, val in input_dict.items():
+
+        if key == "Tut_iterations":
+            if val != "15": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            else: score += 1
+
+        if key == "Tut_steps":
+            if val != "500": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            else: score += 1
+
+        if key == "Tut_delay":
+            if val != "1000": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            else: score += 1
+
+    return render_template("Levels/level2.html",
+        uml_src=url_for('static', filename='assets/SVGs/Level2.svg'),
+        value=score,
+        tips=tips,
+    )
+
+@app.route("/workspace/level3", methods=["POST", "GET"])
+def level3():
+
+    input_dict = {
+        "Tut_iterations": 0,
+        "Tut_steps": 0,
+        "Tut_delay": 0,
+    }
+
+    tips= ""
+    score = 0
+
+    if request.method == "POST":
+
+        for key, val in input_dict.items():
+
+            inp = request.form.get(key)
+            if inp == None: continue
+            else: input_dict[key] = inp
+
+    for key, val in input_dict.items():
+
+        if key == "Tut_iterations":
+            if val != "15": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            else: score += 1
+
+        if key == "Tut_steps":
+            if val != "500": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            else: score += 1
+
+        if key == "Tut_delay":
+            if val != "1000": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            else: score += 1
+
+    return render_template("Levels/level3.html",
+        uml_src=url_for('static', filename='assets/SVGs/Level3.svg'),
+        value=score,
+        tips=tips,
+    )
+
+@app.route("/workspace/level4", methods=["POST", "GET"])
+def level4():
+
+    input_dict = {
+        "Tut_iterations": 0,
+        "Tut_steps": 0,
+        "Tut_delay": 0,
+    }
+
+    tips= ""
+    score = 0
+
+    if request.method == "POST":
+
+        for key, val in input_dict.items():
+
+            inp = request.form.get(key)
+            if inp == None: continue
+            else: input_dict[key] = inp
+
+    for key, val in input_dict.items():
+
+        if key == "Tut_iterations":
+            if val != "15": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            else: score += 1
+
+        if key == "Tut_steps":
+            if val != "500": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            else: score += 1
+
+        if key == "Tut_delay":
+            if val != "1000": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            else: score += 1
+
+    return render_template("Levels/level4.html",
+        uml_src=url_for('static', filename='assets/SVGs/Level4.svg'),
+        value=score,
+        tips=tips,
+    )
+
 
 if __name__ == "__main__":
 
