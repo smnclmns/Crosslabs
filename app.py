3b8d74620ee7d86e8bc88d9eb0febd9c3dd81d06
STIJNDVRS <stijn.devries@tu-dortmund.de>
2023-09-18 11:12:03 +0200

    levle 2

--- app.py
+++ app.py
@@ -216,14 +216,19 @@ def tutorial():
 def level1():
 
     input_dict = {
-        "Tut_calibration_steps": 0,
-        "Tut_calibration_delay": 0,
+        "Boolean_state_1": 0,
+        "Condition_1": 0,
+        "Condition_2": 0,
+        "Boolean_state_2": 0,
         }
     
     tips= ""
     score = 0
+    visible_tips = False
 
     if request.method == "POST":
+
+        visible_tips=True
         
         for key, val in input_dict.items():
 
@@ -233,34 +238,50 @@ def level1():
 
     for key, val in input_dict.items():
         
-        if key == "Tut_calibration_steps":
-            if val != "500": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
-            else: score += 1
+        if key == "Boolean_state_1":
+            if val != "false" and val != "": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            elif val == "false": score += 1
 
-        if key == "Tut_calibration_delay":
-            if val != "1000": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
-            else: score += 1
+        if key == "Condition_1":
+            if val != "<" and val != "": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            elif val == "<": score += 1
+
+        if key == "Boolean_state_2":
+            if val != "true" and val != "": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            elif val == "true": score += 1
+
+        if key == "Condition_2":
+            if val != "<" and val != "": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            elif val == "<": score += 1
 
     return render_template("Levels/level1.html",
         uml_src=url_for('static', filename='assets/SVGs/Level1.svg'),
         uml_src2=url_for('static', filename='assets/SVGs/Level1.1.svg'),
         value=score,
         tips=tips,
+        visible_tips = visible_tips
     )
 
 @app.route("/workspace/level2", methods=["POST", "GET"])
 def level2():
 
     input_dict = {
-        "Tut_iterations": 0,
-        "Tut_steps": 0,
-        "Tut_delay": 0,
+        "Microstep_1":0,
+        "Velocity_1":0,
+        "Velocity_2":0,
+        "Velocity_3":0,
+        "Velocity_4":0,
+        "Velocity_5":0,
+        "Velocity_6":0,
+
     }
 
     tips= ""
     score = 0
+    visible_tips = False
 
     if request.method == "POST":
+        visible_tips=True
 
         for key, val in input_dict.items():
 
@@ -270,37 +291,65 @@ def level2():
 
     for key, val in input_dict.items():
 
-        if key == "Tut_iterations":
-            if val != "15": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
-            else: score += 1
+        if key == "Microstep_1":
+            if val != "1/32" and val != "": tips += f"You didn't set the right Microsteps"
+            elif val == "1/32": score += 1
 
-        if key == "Tut_steps":
-            if val != "500": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
-            else: score += 1
-
-        if key == "Tut_delay":
-            if val != "1000": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
-            else: score += 1
+        if key == "Velocity_1":
+            if val != "0" and val != "": tips += f"You didn't set the right {key}-value.\n"
+            elif val == "0": score += 1
+        if key == "Velocity_2":
+            if val != "0" and val != "": tips += f"You didn't set the right {key}-value.\n"
+            elif val == "0": score += 1
+        if key == "Velocity_3":
+            if not val:
+                tips += f"You didn't set the right {key}-value.\n"
+            elif not val.isdigit() or float(val) >= 10:
+                tips += f"The {key}-value should be a numeric value less than 10.\n"
+            else:
+                score += 1
+        if key == "Velocity_4":
+            if not val:
+                tips += f"You didn't set the right {key}-value.\n"
+            elif not val.isdigit() or float(val) >= 10:
+                tips += f"The {key}-value should be a numeric value less than 10.\n"
+            else:
+                score += 1
+        if key == "Velocity_5":
+            if val != "0" and val != "": tips += f"You didn't set the right {key}-value.\n"
+            elif val == "0": score += 1
+        if key == "Velocity_6":
+            if val != "0" and val != "": tips += f"You didn't set the right {key}-value.\n"
+            elif val == "0": score += 1
 
     return render_template("Levels/level2.html",
-        uml_src=url_for('static', filename='assets/SVGs/Level2.svg'),
+        uml_src=url_for('static', filename='assets/SVGs/Stepper1.svg'),
+        uml_src2=url_for('static', filename='assets/Gifs/fullround.gif'),
         value=score,
         tips=tips,
+        visible_tips=visible_tips,
     )
 
 @app.route("/workspace/level3", methods=["POST", "GET"])
 def level3():
 
     input_dict = {
-        "Tut_iterations": 0,
-        "Tut_steps": 0,
-        "Tut_delay": 0,
+        "Microstep_1":0,
+        "Velocity_1":0,
+        "Velocity_2":0,
+        "Velocity_3":0,
+        "Velocity_4":0,
+        "Velocity_5":0,
+        "Velocity_6":0,
+
     }
 
     tips= ""
     score = 0
+    visible_tips = False
 
     if request.method == "POST":
+        visible_tips=True
 
         for key, val in input_dict.items():
 
@@ -310,17 +359,28 @@ def level3():
 
     for key, val in input_dict.items():
 
-        if key == "Tut_iterations":
-            if val != "15": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
-            else: score += 1
+        if key == "Microstep":
+            if val != "1/32" and val != "": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            elif val == "1/32": score += 1
 
-        if key == "Tut_steps":
-            if val != "500": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
-            else: score += 1
-
-        if key == "Tut_delay":
-            if val != "1000": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
-            else: score += 1
+        if key == "Velocity_1":
+            if val != "0" and val != "": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            elif val == "0": score += 1
+        if key == "Velocity_2":
+            if val != "0" and val != "": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            elif val == "0": score += 1
+        if key == "Velocity_3":
+            if val != "" and val != "": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            elif val < "10": score += 1
+        if key == "Velocity_4":
+            if val != "" and val != "": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            elif val < "10": score += 1
+        if key == "Velocity_5":
+            if val != "0" and val != "": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            elif val == "0": score += 1
+        if key == "Velocity_6":
+            if val != "0" and val != "": tips += f"You didn't set the right {key.split('_')[1]}-value.\n"
+            elif val == "0": score += 1
 
     return render_template("Levels/level3.html",
         uml_src=url_for('static', filename='assets/SVGs/Level3.svg'),
